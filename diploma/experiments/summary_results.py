db24e9436e00f7b042dbb72e182131360238aa59
socloseeee <65871712+socloseeee@users.noreply.github.com>
2023-02-17 00:03:09 +0300

    update

--- diploma/experiments/summary_results.py
+++ diploma/experiments/summary_results.py
@@ -12,6 +12,12 @@ str_methods = (
     "The method of squares | Метод квадратов",
     "The barrier method | Метод барьера"
 )
+gene_form = (
+    "50r+50d",
+    "25r+75d",
+    "75r+25d",
+    "100d"
+)
 colors_dict = {
      'central': 'royalblue',
      'left': 'darkorange',
@@ -20,13 +26,20 @@ colors_dict = {
 }
 bounds = ['central', 'left', 'right', 'random']
 fig, axes = plt.subplots(2, 2)
+fig1, axes1 = plt.subplots(2, 2)
 axes_dict = {
     'central': axes[0][0],
     'left': axes[0][1],
     'right': axes[1][0],
     'random': axes[1][1]
 }
-fig.set_size_inches(19, 9.5)
+axes_dict1 = {
+    'central': axes1[0][0],
+    'left': axes1[0][1],
+    'right': axes1[1][0],
+    'random': axes1[1][1]
+}
+fig.set_size_inches(19, 9.5), fig1.set_size_inches(19, 9.5)
 for bound in bounds:
     with open(f"experiment_results/{bound}_bound/all_result.txt", 'r', encoding="utf-8") as f:
         data = f.readlines()
@@ -37,22 +50,52 @@ for bound in bounds:
         bbox=dict(boxstyle="square", facecolor='darkviolet', edgecolor="black"),
         fontsize=11
     )
-    [
-        axes_dict[bound].bar(
+    [axes_dict[bound].bar(
             [elem[0] for elem in methods_data][i],
             [elem[1] for elem in methods_data][i],
             label=str_methods[i],
             color=colors_dict[[elem for elem in colors_dict.keys()][i]],
             width=(max([elem[0] for elem in methods_data]) - min([elem[0] for elem in methods_data]))/10
         )
-        for i in range(len(methods_data))
-    ]
+    for i in range(len(methods_data))]
+    [axes_dict[bound].text([elem[0] for elem in methods_data][i], [elem[1] for elem in methods_data][i],
+                           f"{[elem[0] for elem in methods_data][i]} | {[elem[1] for elem in methods_data][i]}",
+                     bbox=dict(boxstyle="square", facecolor=colors_dict[[elem for elem in colors_dict.keys()][i]], edgecolor="black"))
+     for i in range(len(methods_data))]
     axes_dict[bound].set_xlabel('Result | Результаты')
     axes_dict[bound].set_ylabel('Elapsed time | Затраченное время')
     axes_dict[bound].set_xlim(right=max([elem[0] for elem in methods_data]) + 10, left=min([elem[0] for elem in methods_data]) - 10)  # 0.05
     axes_dict[bound].set_ylim(bottom=min([elem[1] for elem in methods_data]) - 10, top=max([elem[1] for elem in methods_data]) + 10)
+
+    axes_dict1[bound].set_title(
+        f'{bound}',
+        bbox=dict(boxstyle="square", facecolor='darkviolet', edgecolor="black"),
+        fontsize=11
+    )
+    [axes_dict1[bound].bar(
+        [elem[0] for elem in genes_data][i],
+        [elem[1] for elem in genes_data][i],
+        label=gene_form[i],
+        color=colors_dict[[elem for elem in colors_dict.keys()][i]],
+        width=(max([elem[0] for elem in genes_data]) - min([elem[0] for elem in genes_data])) / 10
+    )
+        for i in range(len(genes_data))]
+    [axes_dict1[bound].text([elem[0] for elem in genes_data][i], [elem[1] for elem in genes_data][i],
+                           f"{[elem[0] for elem in genes_data][i]} | {[elem[1] for elem in genes_data][i]}",
+                           bbox=dict(boxstyle="square", facecolor=colors_dict[[elem for elem in colors_dict.keys()][i]],
+                                     edgecolor="black"))
+     for i in range(len(genes_data))]
+    axes_dict1[bound].set_xlabel('Result | Результаты')
+    axes_dict1[bound].set_ylabel('Elapsed time | Затраченное время')
+    axes_dict1[bound].set_xlim(right=max([elem[0] for elem in genes_data]) + 10,
+                              left=min([elem[0] for elem in genes_data]) - 10)  # 0.05
+    axes_dict1[bound].set_ylim(bottom=min([elem[1] for elem in genes_data]) - 10,
+                              top=max([elem[1] for elem in genes_data]) + 10)
     print(methods_data, genes_data)
 h, l = axes[0][0].get_legend_handles_labels()
+h1, l1 = axes1[0][0].get_legend_handles_labels()
 fig.legend(handles=h, labels=l, loc='upper left')
-plt.savefig("all_bounds_results")
+fig1.legend(handles=h1, labels=l1, loc='upper left')
+fig.savefig("all_methods_results")
+fig1.savefig("all_bounds_results")
 plt.show()
